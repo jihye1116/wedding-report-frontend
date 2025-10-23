7ab788f3ad4ebc9b41872effdf206e30568128a4
jihye1116 <jihyesim170@gmail.com>
2025-10-23 11:40:09 +0900

    feat: update InputField component styles and add new TextAreaField component for multi-line input

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { cn } from "@/utils/cn";
-
 interface InputFieldProps {
   name: string; // 폼 데이터의 키로 사용
   value: string; // 현재 입력 값
@@ -18,8 +16,8 @@ export const InputField = ({
   onClick,
 }: InputFieldProps) => {
   return (
-    <div onClick={onClick} className="w-full rounded-full p-2.5 outline-none">
-      <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-[#111111]">
+    <div onClick={onClick} className="w-full outline-none">
+      <div className="flex items-center rounded-sm bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-[#111111]">
         <input
           type="text"
           name={name}
---
+++ src/components/TextAreaField.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+interface TextAreaFieldProps {
+  name: string; // 폼 데이터의 키로 사용
+  value: string; // 현재 입력 값
+  onChange: (name: string, value: string) => void; // 값 변경 핸들러
+  placeholder: string;
+  rows?: number; // 기본 행 수 설정
+  onClick?: () => void; // 선택적으로 변경
+}
+
+export const TextAreaField = ({
+  name,
+  value,
+  onChange,
+  placeholder,
+  rows = 8, // 기본값 3행
+  onClick,
+}: TextAreaFieldProps) => {
+  return (
+    <div onClick={onClick} className="w-full rounded-xl outline-none">
+      <div className="flex items-start rounded-md pl-4 outline-1 -outline-offset-1 outline-gray-300 has-[textarea:focus-within]:outline-2 has-[textarea:focus-within]:-outline-offset-2 has-[textarea:focus-within]:outline-[#111111]">
+        <textarea
+          name={name}
+          value={value}
+          onChange={(e) => onChange(name, e.target.value)}
+          placeholder={placeholder}
+          rows={rows}
+          className="block w-full resize-none py-5 text-sm text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm"
+        />
+      </div>
+    </div>
+  );
+};
