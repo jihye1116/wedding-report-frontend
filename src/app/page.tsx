16bda4a5442b5eb883bbfadf4e5a3f0e3798ac55
sebanimm <gks32114221@gmail.com>
2025-10-23 09:40:35 +0900

    feat: add Escoredream font faces and apply custom font to body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
       <main className="py-32sm:items-start flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 dark:bg-black">
         <span className="text-white">Home</span>
       </main>
