059396f3b2d99b2cf99047c27ba14e7ebc5bc4f1
sebanimm <gks32114221@gmail.com>
2025-10-23 11:24:08 +0900

    fix: add outline-none class to StartButton for better accessibility

--- src/components/StartButton.tsx
+++ src/components/StartButton.tsx
@@ -4,7 +4,7 @@ import ChevronRight from "@/assets/icons/chevron-right-sm.svg";
 
 export const StartButton = () => {
   return (
-    <button className="flex items-center gap-2 rounded-lg bg-[#6DD4BD] p-2.5 pl-5">
+    <button className="flex items-center gap-2 rounded-lg bg-[#6DD4BD] p-2.5 pl-5 outline-none">
       <span className="text-sm leading-tight font-medium text-white">
         기본 정보 입력
       </span>
