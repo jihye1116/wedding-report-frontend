84b7c16d9da2260e89c4c2f8af0d0448faff6822
jihye1116 <jihyesim170@gmail.com>
2025-10-24 14:24:23 +0900

    refactor: restructure layout in Intro2Page for improved readability and organization
    - Updated the main structure of Intro2Page to separate content and navigation sections.
    - Enhanced styling for better visual hierarchy and user experience.

--- src/pages/result/intro/Intro2Page.tsx
+++ src/pages/result/intro/Intro2Page.tsx
@@ -9,21 +9,26 @@ interface Intro2PageProps {
 
 export default function Intro2Page({ onNext }: Intro2PageProps) {
   return (
-    <main className="flex h-dvh flex-col justify-between px-10">
-      <p className="font-gangwon flex justify-end py-5 text-[#7AC6B6]">
-        꽃길만 걷자
-      </p>
-      <Image
-        className="mx-auto py-5"
-        src={ReportThumbnail}
-        alt="Report Thumbnail"
-      />
-      <article className="font-gangwon text-lg leading-snug font-bold whitespace-pre-wrap text-[#111111]">
-        <p className="text-[#59847B]">남성이름</p>
-        <p className="text-[#59847B]">여성이름</p>
-        <p className="mt-3">만든 날짜</p>
-      </article>
-      <NavigateButton direction="right" onClick={onNext} color={"green"} />
-    </main>
+    <div className="flex h-dvh flex-col justify-between">
+      <div className="px-10">
+        <p className="font-gangwon flex justify-end py-5 font-bold text-[#7AC6B6]">
+          꽃길만 걷자
+        </p>
+        <Image
+          className="mx-auto py-5"
+          src={ReportThumbnail}
+          alt="Report Thumbnail"
+        />
+        <article className="font-gangwon text-lg leading-snug font-bold whitespace-pre-wrap text-[#111111]">
+          <p className="text-[#59847B]">남성이름</p>
+          <p className="text-[#59847B]">여성이름</p>
+          <p className="mt-3">만든 날짜</p>
+        </article>
+      </div>
+
+      <div className="p-10">
+        <NavigateButton direction="right" onClick={onNext} color={"green"} />
+      </div>
+    </div>
   );
 }
